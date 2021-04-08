7ef7e9754b4d245552ac0ccd2c0ab20878009d54
Julio <juliomederosarias@gmail.com>
2021-04-08 18:17:44 -0400

    Vista de solicitudes especialista

--- src/js/views/Requests_client.js
+++ src/js/views/Requests_client.js
@@ -3,50 +3,39 @@ import React from 'react';
 const Requests_client = () => {
 
     return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <h1>Te Ayudo?...con tus solicitudes</h1>
-                    <div className="col-md-6 text-right">
-                        <button type="button" className="btn btn-link">Cerrar sesión</button>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-3">
-                        <div className="card" style={{ height: "8rem" }}>
-                            <img className="card-img-top" src="..." alt="Card image cap" />
-                        </div>
-                        <p>Especialista</p>
-                        <button type="button" className="btn btn-secondary" style={{ width: "12rem" }}>Editar cuenta</button>
-                        <button type="button" className="btn btn-secondary" style={{ width: "12rem" }}>Historial de solicitudes</button>
-                        <button type="button" className="btn btn-secondary" style={{ width: "12rem" }}>Calendario disponible</button>
-                    </div>
-                    <div className="col-sm-6 px-md-4">
-                        <div className="btn-group d-flex flex-row-reverse col-sm-6" role="group" aria-label="Basic example">
-                            <button type="button" className="btn btn-secondary">Disponibilidad</button>
-                            <button type="button" className="btn btn-primary">Solicitudes</button>
-                        </div>
-                        <div className="form-group">
-                            <div className="row px-md-4">
-                                <p>Hola Especialista, estas son tus solicitudes:   </p>
-                                <div className="form-check px-md-5">
-                                    <input
-                                        className="form-check-input"
-                                        type="checkbox"
-                                        value=""
-                                        id="flexCheckDefault"
-                                    />
-                                    <label className="form-check-label" for="flexCheckDefault">
-                                        Mostrar todas
-                                </label>
+        <div className="container">
+            <div className="row">
+                <div className="col-3">
+                    <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <a className="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
+                            <div className="card">
+                                <img src="..." className="card-img-top" alt="..."></img>
+                                <div className="card-body">
+                                    <p className="card-text">Nombre Cliente</p>
                                 </div>
                             </div>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </a>
+                        <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Editar Cuenta</a>
+                        <a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Historial de Solicitudes</a>
+                    </div>
+                </div>
+                <div className="col-9">
+                    <div className="tab-content" id="v-pills-tabContent">
+                        <div className="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                        <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                        <div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">holaaa</div>
+                        <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                            <ul className="list-group">
+                                <li className="list-group-item">Cras justo odio</li>
+                                <li className="list-group-item">Dapibus ac facilisis in</li>
+                                <li className="list-group-item">Morbi leo risus</li>
+                                <li className="list-group-item">Porta ac consectetur ac</li>
+                                <li className="list-group-item">Vestibulum at eros</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
--- src/js/views/Specialist.js
+++ src/js/views/Specialist.js
@@ -2,9 +2,51 @@ import React from 'react';
 
 const Specialist = () => {
     return ( 
-        <diV>
-            <h1>Vista especialista</h1>
-        </diV>
+        <div>
+            <div className="container">
+                <div className="row">
+                    <h1>Te Ayudo?...con tus solicitudes</h1>
+                    <div className="col-md-6 text-right">
+                        <button type="button" className="btn btn-link">Cerrar sesión</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-3">
+                        <div className="card" style={{ height: "8rem" }}>
+                            <img className="card-img-top" src="..." alt="Card image cap" />
+                        </div>
+                        <p>Especialista</p>
+                        <button type="button" className="btn btn-secondary" style={{ width: "12rem" }}>Editar cuenta</button>
+                        <button type="button" className="btn btn-secondary" style={{ width: "12rem" }}>Historial de solicitudes</button>
+                        <button type="button" className="btn btn-secondary" style={{ width: "12rem" }}>Calendario disponible</button>
+                    </div>
+                    <div className="col-sm-6 px-md-4">
+                        <div className="btn-group d-flex flex-row-reverse col-sm-6" role="group" aria-label="Basic example">
+                            <button type="button" className="btn btn-secondary">Disponibilidad</button>
+                            <button type="button" className="btn btn-primary">Solicitudes</button>
+                        </div>
+                        <div className="form-group">
+                            <div className="row px-md-4">
+                                <p>Hola Especialista, estas son tus solicitudes:   </p>
+                                <div className="form-check px-md-5">
+                                    <input
+                                        className="form-check-input"
+                                        type="checkbox"
+                                        value=""
+                                        id="flexCheckDefault"
+                                    />
+                                    <label className="form-check-label" for="flexCheckDefault">
+                                        Mostrar todas
+                                </label>
+                                </div>
+                            </div>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
      );
 }
  
