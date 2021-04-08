b2e897262c8c3ca69d5e305c65d1c68101265d85
Julio <juliomederosarias@gmail.com>
2021-04-07 20:13:16 -0400

    url bootstrap

--- src/js/views/Requests_client.js
+++ src/js/views/Requests_client.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Requests_client = () => {
     return ( 
         <div>
-            <h1>Solicitudes cliente</h1>
+            <h1>Te Ayudo?...con tus solicitudes</h1>
         </div>
      );
 }
