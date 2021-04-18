3fa69a22f95ce69ae3924776a5524ff9e0814b30
antoni perez <antoni.protoni@gmail.com>
2021-04-18 19:02:19 -0400

    home layout fixed

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,63 +1,71 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (  
-        <div className= "container mt-5">
-            <h1 className= "text-center">TeAYUDO?</h1>
-            <hr/>
-            <div className="row">
-                <div className="col-12">
-
-                 <form className="form-inline d-flex justify-content-end">
-
-                            <label className="sr-only" for="inlineFormInputGroupUsername2">Usuario</label>
-                            <div className="input-group mb-2 mr-sm-2">
-                                <div className="input-group-prepend">
-                                <div className="input-group-text">@</div>
-                                </div>
-                                <input type="text" className="form-control" id="inlineFormInputGroupUsername2" placeholder="Usuario"/>
-                            </div>
-
-                            <label className="sr-only" for="inlineFormInputName2">Name</label>
-                            <input type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Contraseña"/>
-                            
-
-                            <button type="submit" className="btn btn-success mb-2">Entrar</button>
-
-                            </form>
-
-
-
-
-
-
-                </div>
-
-
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">TeAYUDO?</h1>
+      <hr />
+      <div className="row">
+        <div className="col-12">
+          <form className="form-inline d-flex justify-content-end">
+            <label className="sr-only" for="inlineFormInputGroupUsername2">
+              Usuario
+            </label>
+            <div className="input-group mb-2 mr-sm-2">
+              <div className="input-group-prepend">
+                <div className="input-group-text">@</div>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                id="inlineFormInputGroupUsername2"
+                placeholder="Usuario"
+              />
             </div>
 
-            <div className="row">
+            <label className="sr-only" for="inlineFormInputName2">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control mb-2 mr-sm-2"
+              id="inlineFormInputName2"
+              placeholder="Contraseña"
+            />
 
-                <div className="col-12">
-
-                            <h5 className="d-flex justify-content-end form-inline ">
-
-                    <span>Registrarse</span>
-                    <span>Olvide Contraseña </span>
-
-                            </h5>
-
-
-                </div>
-
-
-
-            </div>
-
-
+            <button type="submit" className="btn btn-success mb-2">
+              Entrar
+            </button>
+          </form>
         </div>
-    );
-}
- 
+      </div>
+
+      <div className="row mt-3">
+        <div className="col-9 d-flex justify-content-end ">
+          <h5 className=" ">
+            <span>
+              <a href="">
+                {" "}
+                <u>Registrarse </u>{" "}
+              </a>
+            </span>
+          </h5>
+        </div>
+        <div className="col-3 d-flex justify-content-start ">
+          <h5 className=" ">
+            <span>
+              {" "}
+              <a href="">
+                {" "}
+                <u> Olvidé Contraseña </u>
+              </a>
+            </span>
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
