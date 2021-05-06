1536e57e645c3b528fb3c212269af54af7543ce8
Luisana-hub <luisanaviamonte@gmail.com>
2021-05-06 14:16:02 +0000

    configuracion valores iniciales del Formik

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,84 +1,106 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import * as EmailValidator from "email-validator";
 import * as Yup from "yup";
 
 const Home = () => {
-    
+    <Formik
+        initialValues={{ email: "", password: "" }}   // Valores Iniciales
+        onSubmit={(values, { setSubmitting }) => {     // setSubmitting permitira habilitar o deshabilitar el boton Entrar
+            setTimeout(() => {
+                console.log("Logging in", values);
+                setSubmitting(false);
+            }, 500);
+        }}
+    >
+        {props => {
+            const {
+                values,
+                touched,
+                errors,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                handleSubmit
+            } = props;
 
-  return (
-    <div className="container mt-5">
-      <h1 className="text-center">TeAYUDO?</h1>
-      <hr />
-      <div className="row">
-        <div className="col-12">
-          <form className="form-inline d-flex justify-content-end">
-            <label className="sr-only" for="inlineFormInputGroupUsername2">
-              Usuario
-            </label>
-            <div className="input-group mb-2 mr-sm-2">
-              <div className="input-group-prepend">
-                <div className="input-group-text">@</div>
-              </div>
-              <input
-                type="mail"
-                className="form-control"
-                id="inlineFormInputGroupUsername2"
-                placeholder="Mail"
-                name="mail"
-                
-                //onChange={handleInputChange}
-              />
-             
-            </div>
+            return (
+                <div className="container mt-5">
+                    <h1 className="text-center">TeAYUDO?</h1>
+                    <hr />
+                    <div className="row">
+                        <div className="col-12">
+                            <form 
+                            className="form-inline d-flex justify-content-end"
+                            onSubmit={handleSubmit}
+                            >
+                                <label className="sr-only" for="email">
+                                    Usuario
+                                </label>
+                                <div className="input-group mb-2 mr-sm-2">
+                                    <div className="input-group-prepend">
+                                        <div className="input-group-text">@</div>
+                                    </div>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="email"
+                                        placeholder="Ingrese su email"
+                                        name="email"
 
-            <label className="sr-only" for="inlineFormInputName2">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control mb-2 mr-sm-2"
-              id="inlineFormInputName2"
-              placeholder="Contraseña"
-              name="password"
-              
-              //onChange={handleInputChange}
-            />
-            
-            <button type="submit" className="btn btn-success mb-2" >
-              <Link to="/seleccion_usuario" className="text-white">
-                Entrar
-              </Link>
-            </button>
-          </form>
-        </div>
-      </div>
+                                    />
 
-      <div className="row mt-3">
-        <div className="col-9 d-flex justify-content-end ">
-          <h5 className=" ">
-            <span>
-              <Link to="/registro_cliente">
-                <u>Registrarse </u>{" "}
-              </Link>
-            </span>
-          </h5>
-        </div>
-        <div className="col-3 d-flex justify-content-start ">
-          <h5 className=" ">
-            <span>
-              {" "}
-              <a href="">
-                {" "}
-                <u> Olvidé Contraseña </u>
-              </a>
-            </span>
-          </h5>
-        </div>
-      </div>
-    </div>
-  );
+                                </div>
+
+                                <label className="sr-only" for="password">
+                                    Name
+                                </label>
+                                <input
+                                    type="password"
+                                    className="form-control mb-2 mr-sm-2"
+                                    id="password"
+                                    placeholder="Ingrese su contraseña"
+                                    name="password"
+
+                                />
+
+                                <button type="submit" disabled={isSubmitting} className="btn btn-success mb-2" >
+                                    <Link to="/seleccion_usuario" className="text-white">
+                                        Entrar
+                                    </Link>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className="row mt-3">
+                        <div className="col-9 d-flex justify-content-end ">
+                            <h5 className=" ">
+                                <span>
+                                    <Link to="/registro_cliente">
+                                        <u>Registrarse </u>{" "}
+                                    </Link>
+                                </span>
+                            </h5>
+                        </div>
+                        <div className="col-3 d-flex justify-content-start ">
+                            <h5 className=" ">
+                                <span>
+                                    {" "}
+                                    <a href="">
+                                        {" "}
+                                        <u> Olvidé Contraseña </u>
+                                    </a>
+                                </span>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            );   //Finaliza el Return
+        }}
+    </Formik>
+
 };
 
 export default Home;
