eeb32cc44d32a808cf1bcaa166354c3cc365f19d
Luisana-hub <luisanaviamonte@gmail.com>
2021-05-06 09:41:41 +0000

    react hook forms

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
+    const { register, errors, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
   return (
     <div className="container mt-5">
       <h1 className="text-center">TeAYUDO?</h1>
       <hr />
       <div className="row">
         <div className="col-12">
-          <form className="form-inline d-flex justify-content-end">
+          <form className="form-inline d-flex justify-content-end" onSubmit={handleSubmit(onSubmit)}>
             <label className="sr-only" for="inlineFormInputGroupUsername2">
               Usuario
             </label>
@@ -21,8 +25,19 @@ const Home = () => {
                 className="form-control"
                 id="inlineFormInputGroupUsername2"
                 placeholder="Mail"
+                name="mail"
+                ref={
+                    register({
+                        required: {value: true, message: "Ingrese mail de registro"}
+                    }
+
+                    )
+                }
                 //onChange={handleInputChange}
               />
+              <span className="text-danger text-small d-block mb-2">
+                {errors?.mail?.message}
+            </span>
             </div>
 
             <label className="sr-only" for="inlineFormInputName2">
@@ -33,10 +48,20 @@ const Home = () => {
               className="form-control mb-2 mr-sm-2"
               id="inlineFormInputName2"
               placeholder="Contraseña"
+              name="password"
+              ref={
+                    register({
+                        required: {value: true, message: "Ingrese password"}
+                    }
+
+                    )
+                }
               //onChange={handleInputChange}
             />
-
-            <button type="submit" className="btn btn-success mb-2">
+            <span className="text-danger text-small d-block mb-2">
+                {errors?.password?.message}
+            </span>
+            <button type="submit" className="btn btn-success mb-2" >
               <Link to="/seleccion_usuario" className="text-white">
                 Entrar
               </Link>
