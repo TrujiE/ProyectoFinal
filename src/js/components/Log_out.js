247d530cbf20b41b7830417162a11f9bd6cc1d01
Aaron Trujillo <atrujilloembry@gmail.com>
2021-05-07 16:27:30 -0400

    enrrutar a salir

--- src/js/components/Log_out.js
+++ src/js/components/Log_out.js
@@ -5,7 +5,11 @@ import { Link } from "react-router-dom";
 const Log_out = () =>{
 	return (
 	<div className="container ">  		    
-        <h6 style={{textAlign:"right"}}><button type="button" className="btn btn-outline-success my-2 my-sm-2" >Cerrar sesion</button></h6>
+        <h6 style={{textAlign:"right"}}>            
+            <Link to="/" className="btn btn-success my-2 my-sm-2">
+                Cerrar sesion
+            </Link>            
+        </h6>
   	</div>
 	)
 }
