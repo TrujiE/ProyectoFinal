753c785aba8e9857de09d70c4105730b01daeb16
Aaron Trujillo <atrujilloembry@gmail.com>
2021-05-07 20:14:02 -0400

    nivelacion develop

--- src/js/components/Log_out.js
+++ src/js/components/Log_out.js
@@ -6,7 +6,7 @@ const Log_out = () =>{
 	return (
 	<div className="container ">  		    
         <h6 style={{textAlign:"right"}}>            
-            <Link to="/" className="btn btn-success my-2 my-sm-2">
+            <Link to="/" className="btn btn-success my-2 my-sm-2" onClick={ () => window.close()}>
                 Cerrar sesion
             </Link>            
         </h6>
