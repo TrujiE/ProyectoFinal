f1d2beb5e21f963bea44a384a2bb6324be333dd3
Julio <juliomederosarias@gmail.com>
2021-06-05 19:18:20 -0400

    Editar especialidades y comunas, rellenados

--- src/js/components/Nabvar.js
+++ src/js/components/Nabvar.js
@@ -1,7 +1,6 @@
 import React , { useContext }from 'react';
 import { Link, useHistory } from "react-router-dom";
 import { Context } from "../store/appContext";
-import Log_out from "./LogOut";
 
 const Nabvar = () => {
     //<Link className="nav-link" to="/cliente"><Log_out /></Link>
@@ -19,7 +18,6 @@ const Nabvar = () => {
     }
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light row justify-content-between">
-            <Link className="navbar-brand btn btn-outline-success" to="/">Home</Link>
             <div className="navbar-nav">
                 <button style={{ textAlign: "right" }} type="button" className="btn btn-success" onClick={LogOut}
                     >Cerrar sesión</button>
