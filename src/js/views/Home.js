87d39bd3246c4cf127f4b0a092ee9f3473d549dc
Luisana-hub <luisanaviamonte@gmail.com>
2021-06-16 01:22:37 +0000

    correcciones

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -22,13 +22,13 @@ const Home = () => {
                 <br />
                 <br />
                 <p className="lead text-justify"><strong>TeAyudo?® es un nombre ficticio que fue creado para nuestro proyecto de titulación. 
-                La finalidad es entregar cinco servicios domésticos dentro de la región metropolitana, ampliandose a 15 servicios 
-                domesticos que abarcarán las regiones de Valparaíso, Santiago y Concepción en una segunda etapa.
+                La finalidad es entregar cinco servicios domésticos dentro de la región metropolitana, ampliándose a 15 servicios 
+                domésticos que abarcarán las regiones de Valparaíso, Santiago y Concepción en una segunda etapa.
                 Los perfiles habilitados son Cliente y Especialista, ambos deben tener un correo
-                electrónico, un número celular y Rut como rquisitos para el registro.
-                Los horarios habilitados son de 8:00-11:00, 11:00-14:00 y 14:00-17:00.
-                Para los especialistas una vez registrados tendran habilitado 15 días corridos de prueba.</strong></p>
-                <p className="lead text-justify"><strong>Tanto cliente como especialista pueden iniciar sesion desde la misma pagina de ingreso, y
+                electrónico, un número celular y Rut como requisitos para el registro.
+                Los horarios habilitados son de 8:00-11:00 hrs, 11:00-14:00 hrs y 14:00-17:00 hrs.
+                Para los especialistas una vez registrados tendrán habilitado 15 días corridos de prueba.</strong></p>
+                <p className="lead text-justify"><strong>Tanto cliente como especialista pueden iniciar sesión desde la misma página de ingreso, y
                 si deseas crear una cuenta debes registrarte.</strong></p>
                 <hr className="my-4"></hr>
 
